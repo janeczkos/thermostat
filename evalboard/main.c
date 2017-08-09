2fb741a3a87a1feb08448d0284b48ef9c0fd76c9
janeczkos <stanislaw.janeczko@pruftechnik.com.pl>
2017-08-09 13:27:41 +0200

    Encoder setup added. myprint.h guardian added

--- evalboard/main.c
+++ evalboard/main.c
@@ -17,10 +17,12 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <libopencm3/stm32/timer.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/usart.h>
 #include "onewire.h"
+#include "myprint.h"
 
 static void clock_setup(void)
 {
@@ -78,34 +80,54 @@ static void gpio_setup(void)
 		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 }
 
+static void encoder_setup(void)
+{
+     // Use PB6 + PB7 for encoder input (5v)
+             gpio_set_mode    (GPIOB, GPIO_MODE_INPUT,
+          GPIO_CNF_INPUT_PULL_UPDOWN, GPIO6);
+               gpio_set_mode    (GPIOB, GPIO_MODE_INPUT,
+          GPIO_CNF_INPUT_PULL_UPDOWN, GPIO7);
+                timer_reset(TIM4);
+             rcc_periph_clock_enable(RCC_TIM4);
+             timer_set_period(TIM4, 0xFFFF);
+             timer_slave_set_mode(TIM4, TIM_SMCR_SMS_EM3); // encoder
+     
+             timer_ic_set_input(TIM4, TIM_IC1, TIM_IC_IN_TI1);
+             timer_ic_set_input(TIM4, TIM_IC2, TIM_IC_IN_TI2);
+             timer_enable_counter(TIM4);
+}
 int main(void)
 {
 	int i, c = 0;
  	uint8_t temperature[2];	
+    uint32_t pos = 0;
 
 	clock_setup();
 	usart_setup();
 	gpio_setup();
 	ow_usart_setup();
+    encoder_setup();
 
 	while (1) {
 		/* Using API function gpio_toggle(): */
 		gpio_toggle(GPIOC, GPIO13);	/* LED on/off */
-		DebugPrintDecimal( USART2, c++ );
+        pos = timer_get_counter(TIM4);
+        for (i = 0; i < 8000000; i++)_asm__("nop");
+        DebugPrintStr( USART2, "pozycja enkodera: ");
+        DebugPrintDecimal( USART2, pos); 
 		usart_send_blocking( USART2, '\r' );
-                usart_send_blocking( USART2, '\n' );
-
-		if ( OW_CheckPresence() ) {
+        usart_send_blocking( USART2, '\n' );
+		/*if ( OW_CheckPresence() ) {
                         OW_MeasureTemp();
                 }
 
-                for (i = 0; i < 8000000; i++)   /* Wait a bit. */
+                for (i = 0; i < 8000000; i++)   
                         __asm__("nop");
-                for (i = 0; i < 8000000; i++)   /* Wait a bit. */
+                for (i = 0; i < 8000000; i++)   
                         __asm__("nop");
 		if ( OW_CheckPresence() ) {
                         OW_ReadTemp( temperature );
-                }
+                }*/
 	}
 
 	return 0;
--- evalboard/myprint.h
+++ evalboard/myprint.h
@@ -1,3 +1,6 @@
+#ifndef MYPRINT_H
+#define MYPRINT_H
+
 #include <libopencm3/stm32/usart.h>
 
 void DebugPrintDecimal( uint32_t usart, int value );
@@ -7,3 +10,5 @@ void DebugPrintHex32( uint32_t usart, uint32_t data );
 void DebugPrintHex16( uint32_t usart, uint16_t data );
 void DebugPrintHex8( uint32_t usart, uint8_t data );
 void DebugPrintHex4( uint32_t usart, uint8_t data );
+
+#endif //MYPRINT_H
